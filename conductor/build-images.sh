a05c353d66390c3394fd3f90ef86ec90b0eb0338
Kien Nguyen-Tuan <ntk148v@gmail.com>
2022-08-19 00:17:46 +0700

    Update!

--- conductor/build-images.sh
+++ conductor/build-images.sh
@@ -7,17 +7,20 @@ function docker_tag_exists() {
 # Get all releases
 curl -sL https://api.github.com/repos/netflix/conductor/releases | jq -r ".[].tag_name" >/tmp/releases.txt
 
+# Clone
+git clone https://github.com/netflix/conductor.git conductor
+
 while IFS= read -r line; do
     tag=$line
     if docker_tag_exists kiennt26/conductor ${tag}; then
         echo "Docker image kiennt26/conductor:${tag} exist, skip..."
     else
         echo "Docker image kiennt26/conductor:${tag} not exist, let's build it!"
-        # Get release
-        curl -sL https://github.com/Netflix/conductor/archive/refs/tags/${tag}.zip >${tag}.zip
-        unzip -q ${tag}.zip -d ${tag}
+        cd conductor
+        git checkout tags/${tag}
         # Build images
-        docker build -t kiennt26/conductor-serer:${tag} -f ${tag}/conductor-${tag//v}/docker/server/Dockerfile ./${tag}/conductor-${tag//v}
+        docker build -t kiennt26/conductor-serer:${tag} -f docker/server/Dockerfile .
         docker push kiennt26/conductor-serer:${tag}
+        cd -
     fi
 done </tmp/releases.txt
